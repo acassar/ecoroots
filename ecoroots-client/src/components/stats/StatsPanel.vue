f70a336f3f4b4b4c143931fd6b8ebd5aa09db5bd
Adrien Cassar <adricass@live.fr>
2024-11-20 22:20:13 +0100

    pie chart with green area types

--- ecoroots-client/src/components/stats/StatsPanel.vue
+++ ecoroots-client/src/components/stats/StatsPanel.vue
@@ -1,4 +1,51 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { cssVars } from '@/main'
+import type { EChartsOption } from 'echarts'
+import { PieChart } from 'echarts/charts'
+import { LegendComponent, TooltipComponent } from 'echarts/components'
+import { use } from 'echarts/core'
+import { CanvasRenderer } from 'echarts/renderers'
+import { computed } from 'vue'
+import VChart from 'vue-echarts'
+
+use([TooltipComponent, LegendComponent, PieChart, CanvasRenderer])
+
+const props = defineProps<{
+  data: {
+    value: number
+    name: string
+  }[]
+}>()
+
+const option = computed<EChartsOption>(() => {
+  return {
+    tooltip: {
+      trigger: 'item',
+    },
+    legend: {
+      top: '0%',
+      left: 'center',
+    },
+    series: [
+      {
+        name: 'Espaces verts',
+        type: 'pie',
+        radius: ['40%', '70%'],
+        avoidLabelOverlap: false,
+        label: {
+          show: false,
+        },
+        itemStyle: {
+          borderRadius: 5,
+          borderColor: cssVars.getPropertyValue('--color-background'),
+          borderWidth: 5,
+        },
+        data: props.data,
+      },
+    ],
+  }
+})
+</script>
 
 <template>
   <div class="stats-container">
@@ -6,7 +53,9 @@
       <span>
         <h1>Statistiques</h1>
       </span>
-      <div></div>
+      <div>
+        <v-chart class="pie" :option="option" />
+      </div>
     </div>
   </div>
 </template>
@@ -30,11 +79,15 @@
   padding: 0.5rem 2rem;
   margin: var(--panel-margin);
   border-radius: 1rem;
-  background-color: var(--color-background);
   width: var(--panel-width);
 }
 
 .panel > span {
   text-align: center;
+}
+
+.pie {
+  height: 300px;
+  width: 100%;
 }
 </style>
